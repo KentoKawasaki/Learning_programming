8a5d0a3bedd92fee651fac4194a29f2c7774c65e
KentoKawasaki <projectdivakkmd@gmail.com>
2020-08-23 06:29:46 +0900

    Create Laravel project01

---
+++ project01/app/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    // //テーブル名
+    // protected $table = 'tasks';
+    
+    // //可変項目
+    // protected $fillable =
+    // [
+    //     'task',
+    //     'dedline',
+    //     'comment'
+    // ];
+    
+}
